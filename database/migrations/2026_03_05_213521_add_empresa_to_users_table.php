b710cb11523b7308ac33600ee2efcab46614af07
Victor_Alarcon <85529967+Victor-Alarconn@users.noreply.github.com>
2026-03-05 17:23:41 -0500

    Agrega modelo empresa y migraciones multiempresa

---
+++ database/migrations/2026_03_05_213521_add_empresa_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+    $table->foreignId('empresa_id')->nullable()->constrained()->cascadeOnDelete();
+    $table->enum('rol', ['admin','empresa'])->default('empresa');
+    $table->boolean('aprobado')->default(false);
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
