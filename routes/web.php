e9693d45feb7db34ba8cd6032479b64b37202ad9
Emanuel García Rodríguez <emanuelgr150@gmail.com>
2026-03-05 15:32:09 -0500

    uwu

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('cliente.index');
 });
 
 Route::view('/cliente', 'cliente.index')->name('cliente.index');
-Route::view('/pedido', 'pedido.index')->name('pedido.index');
+Route::view('/pedido', 'pedido.index')->name('pedido.index'); 
